b915e6ed2081f2bcf63b9a721ce5b002e1664e3a
Ok Jae Ook <okjaeook98@gmail.com>
2021-07-15 12:12:39 +0900

    환경 설정 및 프로젝트 생성
    SocketClient, SocketServer

---
+++ SocketClient/src/module-info.java
@@ -0,0 +1,5 @@
+module SocketClient {
+	requires javafx.controls;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
---
+++ SocketServer/src/module-info.java
@@ -0,0 +1,5 @@
+module SocketServer {
+	requires javafx.controls;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
