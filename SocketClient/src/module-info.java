e41636a7a9658b5c539846cdb4af01a92edeab86
Ok Jae Ook <okjaeook98@gmail.com>
2021-07-15 17:51:58 +0900

    Update module-info.java

--- SocketClient/src/module-info.java
+++ SocketClient/src/module-info.java
@@ -1,6 +1,7 @@
 module SocketClient {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires java.sql;
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens application.view to javafx.graphics, javafx.fxml;
