83bb6dc30e2135dbefcd59d2277d42b65b092321
Ok Jae Ook <okjaeook98@gmail.com>
2021-07-15 18:05:46 +0900

    login, register 컨트롤러 뼈대 구성

--- SocketClient/src/application/view/LoginController.java
+++ SocketClient/src/application/view/LoginController.java
@@ -1,5 +1,40 @@
 package application.view;
 
+import java.util.HashMap;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+	HashMap<String, Integer> serverConn = new HashMap<String, Integer>();
+
+	@FXML
+	private TextField emailText;
+
+	@FXML
+	private TextField pwText;
+
+	@FXML
+	private ChoiceBox<String> serverChoice;
+
+	@FXML
+	private Button loginBtn;
+
+	@FXML
+	private Button registerBtn;
+
+	// 로그인 이벤트
+	@FXML
+	private void loginEvent() {
+
+	}
+
+	// 회원가입 이벤트
+	@FXML
+	private void registerEvent() {
+
+	}
 
 }
--- SocketClient/src/application/view/RegisterController.java
+++ SocketClient/src/application/view/RegisterController.java
@@ -1,5 +1,35 @@
 package application.view;
 
-public class RegisterController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
 
+public class RegisterController {
+	@FXML
+	private TextField emailText;
+
+	@FXML
+	private TextField pwText;
+
+	@FXML
+	private CheckBox confirmCheck;
+
+	@FXML
+	private Button applyBtn;
+
+	@FXML
+	private Button cancelBtn;
+
+	// 등록 이벤트
+	@FXML
+	private void applyEvent() {
+
+	}
+
+	// 취소 이벤트
+	@FXML
+	private void cancelEvent() {
+
+	}
 }
