96433cec669e37231d5d04d20b58d99a446bc26c
Ok Jae Ook <okjaeook98@gmail.com>
2021-07-16 16:58:56 +0900

    Create UserVO.java

---
+++ SocketClient/src/application/model/UserVO.java
@@ -0,0 +1,32 @@
+package application.model;
+
+public class UserVO {
+	private String email;
+	private String nick;
+	private String pw;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNick() {
+		return nick;
+	}
+
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+}
