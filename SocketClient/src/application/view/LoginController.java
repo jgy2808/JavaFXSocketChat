64de3adbbf22f963c456a6bc8bbac64fcd356fff
Ok Jae Ook <okjaeook98@gmail.com>
2021-07-16 17:31:11 +0900

    ComboBox item 추가
    ChoiceBox로 오인하여 사용했었음. 정정함

--- SocketClient/src/application/view/LoginController.java
+++ SocketClient/src/application/view/LoginController.java
@@ -7,7 +7,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -16,7 +16,7 @@ public class LoginController {
 	HashMap<String, Integer> serverConn = new HashMap<String, Integer>();
 	DBConnect connector = new DBConnect();
 	Main scene = new Main();
-	
+
 	@FXML
 	private TextField emailText;
 
@@ -24,51 +24,59 @@ public class LoginController {
 	private PasswordField pwText;
 
 	@FXML
-	private ChoiceBox<String> serverChoice;
+	private ComboBox<String> serverList;
 
 	@FXML
 	private Button loginBtn;
 
 	@FXML
 	private Button registerBtn;
-	
 
 	// 로그인 이벤트
 	@FXML
 	private void loginEvent() {
 		int result;
-		Stage stage = (Stage)loginBtn.getScene().getWindow();
+		Stage stage = (Stage) loginBtn.getScene().getWindow();
 		Alert alert;
 		connector.connect();
 		String email = emailText.getText();
 		String passwd = pwText.getText();
 		result = connector.login(email, passwd);
-		switch(result) {
-			case 1:
-				scene.mainScene(stage);
-				break;
-			case 2:
-				alert = new Alert(AlertType.WARNING);
-				alert.setTitle("로그인 실패");
-				alert.setHeaderText("로그인 실패");
-				alert.setContentText("비밀번호가 틀렸습니다.");
-				alert.showAndWait();
-				break;
-			case 3:
-				alert = new Alert(AlertType.WARNING);
-				alert.setTitle("로그인 실패");
-				alert.setHeaderText("로그인 실패");
-				alert.setContentText("없는 이메일입니다.");
-				alert.showAndWait();
-				break;
+		switch (result) {
+		case 1:
+			scene.mainScene(stage);
+			break;
+		case 2:
+			alert = new Alert(AlertType.WARNING);
+			alert.setTitle("로그인 실패");
+			alert.setHeaderText("로그인 실패");
+			alert.setContentText("비밀번호가 틀렸습니다.");
+			alert.showAndWait();
+			break;
+		case 3:
+			alert = new Alert(AlertType.WARNING);
+			alert.setTitle("로그인 실패");
+			alert.setHeaderText("로그인 실패");
+			alert.setContentText("없는 이메일입니다.");
+			alert.showAndWait();
+			break;
 		}
 	}
 
 	// 회원가입 이벤트
 	@FXML
 	private void registerEvent() {
-		Stage stage = (Stage)registerBtn.getScene().getWindow();
+		Stage stage = (Stage) registerBtn.getScene().getWindow();
 		scene.registerScene(stage);
+	}
+
+	// 서버 선택 이벤트
+	@FXML
+	private void serverChoiceEvent() {
+		String server = serverList.getValue();
+
+		String IP = server.split(":")[0];
+		int port = Integer.parseInt(server.split(":")[1]);
 	}
 
 }
